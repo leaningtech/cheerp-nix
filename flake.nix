24d50b009a814b7092803573b26ea1b61dcfa656
Yuri Iozzelli <y.iozzelli@gmail.com>
2025-05-30 15:14:57 +0200

    update nixpkgs to 25.05

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
   };
 
   outputs = { self, nixpkgs, ... }@inputs: inputs.utils.lib.eachSystem [
