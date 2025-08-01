926313a1933be3e6f439d9f87a28fbc283d4e8b3
Yuri Iozzelli <y.iozzelli@gmail.com>
2025-08-01 12:46:11 +0200

    run unit tests in separate derivations

--- cheerp/unit-tests.nix
+++ cheerp/unit-tests.nix
@@ -1,25 +1,47 @@
-{ lib, stdenv, python3, nodejs, cheerp, sources, filterSrc }:
+{ lib, stdenv, symlinkJoin, python3, nodejs, cheerp, sources, filterSrc }:
+let
+  doTest = { set, asan }: stdenv.mkDerivation {
+    pname = "cheerp-unit-tests-${set}${if asan then "-asan" else ""}";
+    version = "master";
 
-stdenv.mkDerivation {
-  pname = "cheerp-unit-tests";
-  version = "master";
+    src = filterSrc {
+      root = sources.cheerp-utils;
+      include = [ "tests" ];
+    };
 
-  src = filterSrc {
-    root = sources.cheerp-utils;
-    include = [ "tests" ];
+    nativeBuildInputs = [ python3 nodejs cheerp ];
+
+    configurePhase = "";
+    buildPhase = ''
+      cd tests
+      python3 run_tests.py -j $NIX_BUILD_CORES --${set} ${if asan then "--asan" else ""} clang++ node
+    '';
+    installPhase = ''
+      mkdir -p $out
+      cp testOut.out $out/
+      cp testReport.test $out/
+    '';
+
   };
-
-  nativeBuildInputs = [ python3 nodejs cheerp ];
-
-  configurePhase = "";
-  buildPhase = ''
-    cd tests
-    python3 run_tests.py -j $NIX_BUILD_CORES --all --asan clang++ node
-  '';
-  installPhase = ''
-    mkdir -p $out
-    cp testOut.out $out/
-    cp testReport.test $out/
-  '';
-
-}
+  variants = [
+    { set = "genericjs"; asan = false; }
+    { set = "preexecute"; asan = false; }
+    { set = "asmjs"; asan = false; }
+    { set = "asmjs"; asan = true; }
+    { set = "wasm"; asan = false; }
+    { set = "wasm"; asan = true; }
+    { set = "preexecute-asmjs"; asan = false; }
+    { set = "preexecute-asmjs"; asan = true; }
+  ];
+  tests = builtins.listToAttrs (map
+    (opts: {
+      name = "${opts.set}${if opts.asan then "-asan" else ""}";
+      value = doTest opts;
+    })
+    variants);
+in
+symlinkJoin
+  {
+    name = "cheerp-unit-tests";
+    paths = builtins.attrValues tests;
+  } // tests
