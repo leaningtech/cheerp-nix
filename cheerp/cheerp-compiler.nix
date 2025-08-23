04c917ea4665771fdcc04bffe701cb9d60dfe9ea
Yuri Iozzelli <y.iozzelli@gmail.com>
2025-08-23 16:22:36 +0200

    NFC: improve cheerp-compiler nix code

--- cheerp/cheerp-compiler.nix
+++ cheerp/cheerp-compiler.nix
@@ -3,6 +3,26 @@
 let
   build-type = if conf.build == "prod" then "Release" else "Debug";
   doCheck = conf.doCheck && !buildClangd;
+  cmakeListToString = l: lib.concatStringsSep ";" l;
+  cmakeList = name: val: lib.cmakeOptionType "list" name (cmakeListToString val);
+  llvm-projects = [
+    "clang"
+  ] ++ (if buildClangd then [ "clang-tools-extra" ] else []);
+  toolchain-tools = [
+    "llvm-dis"
+    "llvm-ar"
+    "llvm-link"
+    "opt"
+    "llc"
+  ];
+  distribution-components = toolchain-tools ++ [
+    "clang"
+    "clang-resource-headers"
+  ] ++ (if buildClangd then [ "clangd" ] else []);
+  targets = [
+    "X86"
+    "WebAssembly"
+  ];
 in stdenv.mkDerivation {
   pname = if buildClangd then "cheerp-clangd" else "cheerp-compiler";
   version = "${conf.build}-master";
@@ -44,41 +64,48 @@ in stdenv.mkDerivation {
   '' else
     "";
 
-  configurePhase = ''
-    cmake \
-    -DCMAKE_INSTALL_PREFIX=$out \
-    -DLLVM_ENABLE_PROJECTS="clang${
-      if buildClangd then ";clang-tools-extra" else ""
-    }" \
-    -GNinja \
-    -DCMAKE_BUILD_TYPE=${build-type} \
-    -DCMAKE_COLOR_DIAGNOSTICS=ON \
-    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
-    -DCMAKE_C_FLAGS="-Wno-gnu-line-marker" \
-    -DCMAKE_CXX_FLAGS="-Wno-gnu-line-marker" \
-    -DCMAKE_C_FLAGS_RELEASE="-O2 -DNDEBUG" \
-    -DCMAKE_CXX_FLAGS_RELEASE="-O2 -DNDEBUG -Wno-deprecated-declarations" \
-    -DCMAKE_C_FLAGS_DEBUG="-O2" \
-    -DCMAKE_CXX_FLAGS_DEBUG="-O2 -Wno-deprecated-declarations" \
-    -DCMAKE_EXE_LINKER_FLAGS=-fuse-ld=lld \
-    -DCMAKE_SHARED_LINKER_FLAGS=-fuse-ld=lld \
-    -DCMAKE_MODULE_LINKER_FLAGS=-fuse-ld=lld \
-    -DLLVM_USE_RELATIVE_PATHS_IN_DEBUG_INFO=ON \
-    -DLLVM_USE_RELATIVE_PATHS_IN_FILES=ON \
-    -DLLVM_INCLUDE_TESTS=${if doCheck then "ON" else "OFF"} \
-    -C llvm/CheerpCmakeConf.cmake -B build -S llvm
-    cd build
+  cmakeBuildType = build-type;
+  # workaround for option values with spaces
+  preConfigure = ''
+    cmakeFlagsArray+=(
+      "-DCMAKE_CXX_FLAGS='-Wno-gnu-line-marker -Wno-deprecated-declarations'"
+      "-DCMAKE_C_FLAGS_RELEASE='-O2 -DNDEBUG'"
+      "-DCMAKE_CXX_FLAGS_RELEASE='-O2 -DNDEBUG'"
+    )
+    cd llvm
   '';
+  cmakeFlags = [
+    "-GNinja"
+    (cmakeList "LLVM_ENABLE_PROJECTS" llvm-projects)
+    (lib.cmakeBool "CMAKE_COLOR_DIAGNOSTICS" true)
+    (lib.cmakeBool "CMAKE_EXPORT_COMPILE_COMMANDS" true)
+    (lib.cmakeFeature "CMAKE_C_FLAGS" "-Wno-gnu-line-marker")
+    (lib.cmakeFeature "CMAKE_C_FLAGS_DEBUG" "-02")
+    (lib.cmakeFeature "CMAKE_CXX_FLAGS_DEBUG" "-02")
+    (lib.cmakeFeature "CMAKE_EXE_LINKER_FLAGS" "-fuse-ld=lld")
+    (lib.cmakeFeature "CMAKE_SHARED_LINKER_FLAGS" "-fuse-ld=lld")
+    (lib.cmakeFeature "CMAKE_MODULE_LINKER_FLAGS" "-fuse-ld=lld")
+    (lib.cmakeBool "LLVM_USE_RELATIVE_PATHS_IN_DEBUG_INFO" true)
+    (lib.cmakeBool "LLVM_USE_RELATIVE_PATHS_IN_FILES" true)
+    (lib.cmakeBool "LLVM_INCLUDE_TESTS" doCheck)
+    (lib.cmakeFeature "LLVM_DEFAULT_TARGET_TRIPLE" "cheerp-leaningtech-webbrowser-wasm")
+    (lib.cmakeBool "LLVM_INSTALL_TOOLCHAIN_ONLY" true)
+    (cmakeList "LLVM_TARGETS_TO_BUILD" targets)
+    (cmakeList "LLVM_TOOLCHAIN_TOOLS" toolchain-tools)
+    (cmakeList "LLVM_DISTRIBUTION_COMPONENTS" distribution-components)
+    (lib.cmakeBool "CLANG_ENABLE_STATIC_ANALYZER" false)
+    (lib.cmakeBool "CLANG_ENABLE_ARCMT" false)
+  ];
   buildPhase = ''
-    TERM=dumb ninja -j $NIX_BUILD_CORES ${if buildClangd then "clangd" else "distribution"}
+    TERM=dumb ninja -j$NIX_BUILD_CORES ${if buildClangd then "clangd" else "distribution"}
   '';
   checkPhase = ''
-    TERM=dumb ninja -j $NIX_BUILD_CORES check-llvm
-    TERM=dumb ninja -j $NIX_BUILD_CORES check-clang
+    TERM=dumb ninja -j$NIX_BUILD_CORES check-llvm
+    TERM=dumb ninja -j$NIX_BUILD_CORES check-clang
   '';
-  installPhase = ''
-    TERM=dumb ninja ${if buildClangd then "install-clangd" else "install-distribution"}
-  '';
+  installTargets = [
+    (if buildClangd then "install-clangd" else "install-distribution")
+  ];
 
   meta = with lib; {
     homepage = "https://github.com/leaningtech/cheerp-compiler";
